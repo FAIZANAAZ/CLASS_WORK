0773023791f04457094d3359bcdbb526ba2298f7
FAIZA NAAZ <160342268+FAIZANAAZ@users.noreply.github.com>
2024-10-26 03:35:42 +0500

    ubdate

--- CLASS_6/figma-web/src/app/Front/page.tsx
+++ CLASS_6/figma-web/src/app/Front/page.tsx
@@ -3,6 +3,18 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
+import { CiHeart } from "react-icons/ci";
+import { IoMenu } from "react-icons/io5";
+import { PiGreaterThan } from "react-icons/pi";
+import { PiLessThan } from "react-icons/pi";
+import { MdOutlinePhoneAndroid } from "react-icons/md";
+import { IoWatchOutline } from "react-icons/io5";
+import { CiCamera } from "react-icons/ci";
+import { HiOutlineComputerDesktop } from "react-icons/hi2";
+import { LuHeadphones } from "react-icons/lu";
+import { MdOutlineVideogameAsset } from "react-icons/md"
+import { PiDotsThreeOutlineLight } from "react-icons/pi";
+
 
 function Front() {
   let sec2: any = [
@@ -39,27 +51,28 @@ function Front() {
   let sec3: any = [
     {
       p1: "Phones",
-      icon: "",
+      icon: <MdOutlinePhoneAndroid />,
     },
     {
       p1: "Smart Watches",
-      icon: "",
+      icon: <IoWatchOutline />,
     },
     {
       p1: "Cameras",
-      icon: "",
+      icon: <CiCamera />,
     },
     {
       p1: "Head phones",
-      icon: "",
+   
+      icon:   <LuHeadphones /> ,
     },
     {
       p1: "Computers",
-      icon: "",
+      icon: <HiOutlineComputerDesktop />,
     },
     {
       p1: "Gaming",
-      icon: "",
+      icon: <MdOutlineVideogameAsset />,
     },
   ];
 
@@ -141,6 +154,48 @@ function Front() {
     },
   ];
 
+  // sec6
+
+  let sec6: any = [
+    {
+      p1: "Apple iPhone 14  ",
+      p2: " Pro 512GB Gold",
+      price: "$900",
+      pic: "/iphon1sec6.png",
+     
+     
+    
+    },
+    {
+      p1: "AirPods Max ",
+      p2:"Silver",
+      price: "$2535",
+      pic: "/earphon1sec4.png",
+      wirth: 192,
+     
+    
+    },
+    {
+      p1: "Apple Watch Series ",
+      p2 :"9 GPS 41mm Starlight ",
+      price: "$399",
+      pic: "/wattch1sec4.png",
+      wirth: 192,
+     
+    
+    },
+    {
+      p1: "Apple iPhone 14   ",
+      p2:"Pro 1TB Gold",
+      price: "$549",
+      pic: "/iphon2sec6.png",
+      wirth: 192,
+     
+    
+    }
+    
+  ];
+
   return (
     <>
       <Head>
@@ -148,12 +203,13 @@ function Front() {
         <title>figma-base-web</title>
       </Head>
       <main className="w-[375px] ">
-        <header className="h-[88px] w-[375px] bg-white  flex items-center px-[16px] font-bold ">
-          <h1 className="text-[20px]">cyber</h1>
+        <header className="h-[88px] w-[375px] bg-white  flex items-center justify-between px-[16px] font-bold ">
+          <h1 className="text-[30px]">cyber</h1>
 
           {/* Icon with Correct Classes */}
-          {/* <div className="text-black">
-        </div> */}
+          <div className="text-black text-[30px]">
+          <IoMenu />
+        </div> 
         </header>
 
         {/* sec-1 */}
@@ -255,10 +311,13 @@ function Front() {
 
         {/* sec-3 */}
         <section className="relative bg-[#FAFAFA] h-[624px] gap-[48px] py-[64px] px[16px]">
-          <div className="absolute left-[16px] t-[64px] h-[32px] w-[343px] text-center">
+          <div className="absolute left-[16px] t-[64px] h-[32px] w-[343px] text-center flex flex-row justify-between">
             <h1 className="font-medium text-[24px] leading-[32px] tracking-[1%]">
-              Browse By Category
+              Browse By Category 
             </h1>
+            <h2 className="text-[30px] font-bold"><PiLessThan /> </h2>
+            <h2 className="text-[30px] font-bold">  <PiGreaterThan /></h2>
+
           </div>
 
           <div className="iconsparentdiv w-[343px] h-[416px] absolute left-[16px] top-[144px] flex flex-wrap gap-[16px] ">
@@ -266,7 +325,7 @@ function Front() {
             {sec3.map((icon:any,index:number)=>{
               return(
                <div key={index} className="icondiv rounded-[15px] relative bg-[#EDEDED] w-[163.5px] h-[128px] flex items-center justify-center  gap-y-[8px] px-[52px] py-[24px]">
-               <div className="w-[48px] h-[48px] bg-black mb-[15px]"></div>
+               <div className="w-[48px] h-[48px] text-[50px] mb-[15px]">{icon.icon}</div>
                <p className="font-medium text-center text-[16px] leading-[24px] absolute  top-[85px]">
                  {icon.p1}
                </p>
@@ -278,7 +337,7 @@ function Front() {
 
         {/* sec4 */}
 
-        <section className="sec4 w-[375px] h-[1632px] gap-y-[32px] px-[16px]  py-[56px] bg-[#FFFFFF] relative">
+        <section className=" sec4 w-[375px] h-[1612px] gap-y-[32px] px-[16px]  py-[56px] bg-[#FFFFFF] relative">
           <div className="topdiv w-[343px] h-[32px] gap-x-[12px] flex justify-evenly  absolute top-[56px] left-[16px]   text-[16px] font-medium ">
             <a href="#">New Arrival</a>
             <a href="#" className="text-[#8B8B8B]">Bestseller</a>
@@ -286,12 +345,12 @@ function Front() {
           
           </div>
 
-          <div className="buttomdiv w-[343px] h-[1456px] absolute top-[100px] flex flex-wrap">
+          <div className="buttomdiv w-[343px]   h-[1456px] absolute  top-[100px] flex flex-wrap ">
 
             {sec4.map((items:any, index:any)=>{
               return(
-                <div key={index} className="w-[163.5px] h-[332px] ml-[5px] pl-[20px] px-[12px] py-[24px] bg-[#F6F6F6] rounded-[9px] ">
-                <div className="w-[90%] h-[28px] bg-black  gap-x-[8px] mb-[8px]"></div>
+                <div key={index} className="abcdiv w-[163.5px]  h-[332px] ml-[5px] pl-[20px] px-[12px] py-[24px] bg-[#f6f6f6] rounded-[9px] ">
+                <div className="ml-[100px] h-[28px] text-end text-[20px] gap-x-[8px] mb-[8px]"><CiHeart /></div>
                 <Image src={items.pic} alt={"phon"} width={104} height={104} className="w-[104px] h-[104px]"></Image>
                 <pre className="leading-[18px] text-center font-semibold text-[14px] mt-[15px]">{items.p1}<br/>
               {items.p2}</pre>
@@ -308,7 +367,64 @@ function Front() {
 
 
         </section>
+        {/* sec5 */}
+
+        <section className="flex flex-col  items-center h-[700px] w-[375px] gap-y-[48px] px-[32px] py-[56px] bg-[#F9F9F9] ">
+       
+          <div className="w-[311px] h-[555px] gap-y-[16px] ">
+            <Image
+              src={"/sec5.png"}
+              alt={`Image of com`}
+              width={321}
+              height={331}
+              className=" h-[321px] w-[331px] "
+            ></Image>
+            <div className="w-[311px] h-[48px] flex flex-col items-center gap-y-[20px] leading-[48px] text-center">
+              <p className="text-[49px] mt-[20px] text-center font-light ">Ipad Pro</p>
+
+              <p className="leading-[24px]  text-[14px] font-medium text-[#909090] text-center">
+              iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.
+              </p>
+              <button className="w-[191px] h-[56px]  gap-[8px] rounded-[6px] font-[600] text-black border-solid border-[2px] border-[#3f3434]">
+                Shop Now
+              </button>
+
+              
+           
+            </div>
+            
+            </div>
+            <h1 className="text-[30px]"><PiDotsThreeOutlineLight /></h1>
+
+        </section>
+
+        {/* sec6 */}
+        <section className="h-[896px] w-[375px] flex flex-col gap-y-[32px] px-[16px] py-[56px] bg-[#FFFFFF] ">
+          <h1 className="text-start font-medium text-[24px] leading-[32px] text-[#000000] ">
+          Discounts up to -50%
+          </h1>
+<div className="w-[343px] h-[720px] flex  flex-wrap gap-y-[16px] ">
+
+{sec6.map((items:any, index:any)=>{
+              return(
+                <div key={index} className=" w-[163.5px]  h-[332px] ml-[5px] pl-[20px] px-[12px] py-[24px] bg-[#f6f6f6] rounded-[9px] ">
+                <div className="ml-[100px] h-[28px] text-end text-[20px] gap-x-[8px] mb-[8px]"><CiHeart /></div>
+                <Image src={items.pic} alt={"phon"} width={104} height={104} className="w-[104px] h-[104px]"></Image>
+                <pre className="leading-[18px] text-center font-semibold text-[14px] mt-[15px]">{items.p1}<br/>
+              {items.p2}</pre>
+               <h1 className="leading-[24px] tracking-[3%] font-bold text-[24px] text-center mt-[10px]">{items.price}</h1>
+    
+               <button className="gap-x-[8px] px-[44px] py-[12px] mt-[20px] bg-black text-[10px] text-white rounded-[8px] ">Buy Now</button>
+
+                </div>
+              )
+            })}
+</div>
+        </section>
+
       </main>
+
+      
     </>
   );
 }
--- CLASS_6/figma-web/src/app/layout.tsx
+++ CLASS_6/figma-web/src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { FaFacebookF } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa6";
+import { PiTiktokLogoLight } from "react-icons/pi";
+import { AiFillInstagram } from "react-icons/ai";
+import { BiLogoTiktok } from "react-icons/bi";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -24,12 +28,78 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          
-        {children}
-      </body>
-    </html>
+    <>
+      <html>
+        <body>
+          {children}
+
+          <footer
+            className="h-[814.87px] bg-black flex flex-col items-center gap-y-[32px] px-[32px] py-[48px]">
+            <div className="fdiv w-[311px]  h-[662.87px] flex flex-col gap-y-[32px] ">
+              <div className="topdiv   w-[311px] h-[86.87px] flex flex-col gap-y-[16px] items-center justify-center font-bold">
+                <h1 className="text-white text-[25px]">cyber</h1>
+                <p className="font-semibold text-[white] leading-[20px] text-[13px] text-center ">
+                  We are a residential interior design firm located in <br />
+                  Portland. Our boutique-studio offers more than
+                </p>{" "}
+              </div>
+              <div className="bdiv w-[311px] h-[544px] flex flex-col gap-y-[32px] ">
+                <div className="div1 h-[256px] w-[311px] flex flex-col items-center gap-y-[8px] text-center  ">
+                  <h1 className="text-white text-[16px] font-semibold leading-[16px] ">
+                    Services
+                  </h1>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Bonus program
+                  </h2>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Gift cards
+                  </h2>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Credit and payment
+                  </h2>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Service contracts
+                  </h2>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Non-cash account
+                  </h2>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Payment
+                  </h2>
+                </div>
+
+                <div className="div2 h-[256px] w-[311px] flex flex-col items-center gap-y-[8px] text-center ">
+                  <h1 className="text-white text-[16px] font-semibold leading-[16px] ">
+                    Assistance to the buyer
+                  </h1>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Find an order
+                  </h2>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Terms of delivery
+                  </h2>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Exchange and return of goodst
+                  </h2>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Guarantee
+                  </h2>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Frequently asked questions
+                  </h2>
+                  <h2 className="text-[#CFCFCF] leading-[32px] text-[14px] ">
+                    Terms of use of the site
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <div className="lastdiv w-[200px] h-[24px] flex justify-evenly text-[20px] text-white "><a href="#"><FaTwitter /></a>
+            <a href="#" ><FaFacebookF /></a>
+            <a href="#"><BiLogoTiktok /></a>
+            <a href="#"><AiFillInstagram  className="text-white"/></a></div>  
+          </footer>
+        </body>
+      </html>
+    </>
   );
 }
